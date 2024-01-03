d61650963e245c45ec6f7512d67da46223194387
Anshu Bhatia <abusaco@gmail.com>
2024-01-03 11:09:58 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+           <h1> This is My React Application </h1>
         </a>
       </header>
     </div>
